ebed35fef9e040543864cd597b7344edde33cfeb
ayush gajera <ayushgajera50@gmail.com>
2025-04-06 23:48:38 +0530

    linear search in array

--- arrayBasic.java
+++ arrayBasic.java
@@ -5,9 +5,23 @@ public class arrayBasic {
             System.out.println(num[i]);
         }
     }
+
+    public static int linearSearch(int num[],int key){
+        for(int i=0;i<num.length;i++)
+        {
+            if (num[i]==key) {
+                return i;
+            }
+        }
+        System.out.println("no key found");
+        return -1;
+    }
      public static void main(String[] args) {
         int num[]={10,2,3,4,5};
-        
-        displayarray(num);
+        int key=3;
+        // displayarray(num);
+        int number=linearSearch(num,key);
+        System.out.println(number);
+
      }
 }
