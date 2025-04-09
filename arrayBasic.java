0f11549644c3623076c37329ab16f2a640f2bde5
ayush gajera <ayushgajera50@gmail.com>
2025-04-09 12:09:54 +0530

    binary search

--- arrayBasic.java
+++ arrayBasic.java
@@ -5,7 +5,6 @@ public class arrayBasic {
             System.out.println(num[i]);
         }
     }
-
     public static int linearSearch(int num[],int key){
         for(int i=0;i<num.length;i++)
         {
@@ -16,12 +15,28 @@ public class arrayBasic {
         System.out.println("no key found");
         return -1;
     }
+    public static int binarysearch(int num[],int key){
+        int start=0,end=num.length-1;
+        while (start<=end) {
+            int mid=(start+end)/2;
+            if (num[mid]==key) {
+                return mid;
+            }
+            if (num[mid]<key) {
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+        }
+        return -1;
+    }
+
      public static void main(String[] args) {
-        int num[]={10,2,3,4,5};
-        int key=3;
+        int num[]={2,4,6,8,10,12,14};
+        int key=12;
         // displayarray(num);
-        int number=linearSearch(num,key);
-        System.out.println(number);
+       System.out.println("index key is:" +binarysearch(num,key));
 
      }
 }
