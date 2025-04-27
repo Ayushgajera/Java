c1617eebb579fadcc191c5fee4f935d7895f0829
ayush gajera <ayushgajera50@gmail.com>
2025-04-27 16:25:43 +0530

    min & max in subarray

--- arrayBasic.java
+++ arrayBasic.java
@@ -5,20 +5,7 @@ public class arrayBasic {
             System.out.println(num[i]);
         }
     }
-    public static void countEvenOdd(int[] arr) {
-        int even = 0, odd = 0;
-
-        for (int num : arr) {
-            if (num % 2 == 0) {
-                even++;
-            } else {
-                odd++;
-            }
-        }
-
-        System.out.println("Even numbers: " + even);
-        System.out.println("Odd numbers: " + odd);
-    }
+   
     public static int linearSearch(int num[],int key){
         for(int i=0;i<num.length;i++)
         {
@@ -51,7 +38,6 @@ public class arrayBasic {
         int num[]={2,4,6,8,10,12,14};
         int key=12;
         // displayarray(num);
-        countEvenOdd(num);
         // System.out.println("index key is:" +binarysearch(num,key));
 
      }
--- arrayquestion.java
+++ arrayquestion.java
@@ -1,43 +1,66 @@
+import java.util.*;
+
 public class arrayquestion {
-    public static void reverseArray(int num[]){
-        int first=0,last=num.length-1;
-        while (first<last) 
-        {
-            int temp=num[last];
-            num[last]=num[first];
-            num[first]=temp;
+    public static void reverseArray(int num[]) {
+        int first = 0, last = num.length - 1;
+        while (first < last) {
+            int temp = num[last];
+            num[last] = num[first];
+            num[first] = temp;
 
             first++;
             last--;
 
         }
     }
-    public static void pairsInArray(int num[])
-    {
-        for(int i=0;i<num.length;i++)
-        {
-            
-            for(int j=i+1;j<num.length;j++)
-            {
-                System.out.print("("+ num[i] +","+num[j]+")");
+
+    public static void pairsInArray(int num[]) {
+        for (int i = 0; i < num.length; i++) {
+
+            for (int j = i + 1; j < num.length; j++) {
+                System.out.print("(" + num[i] + "," + num[j] + ")");
             }
             System.out.println();
         }
 
     }
 
-    public static void subarray(int num[]){
-        
+    public static void subarray(int num[]) {
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < num.length; i++) {
+
+            for (int j = i + 1; j < num.length; j++) {
+                int sum = 0;
+
+                for (int k = i; k <= j; k++) {
+                    sum += num[k];
+                    System.out.print(num[k] + "");
+                }
+                System.out.print("  sum of subarray is :" + sum);
+                if (sum < min) {
+                    min = sum;
+                }
+                if (sum > max) {
+                    max = sum;
+                }
+                System.out.println();
+            }
+            System.err.println();
+        }
+        System.out.println("min of subarray:" + min);
+        System.out.println("max of subarray:" + max);
+
     }
-    public static void main(String args[])
-    {
-        int num[]={2,4,6,8,10};
+
+    public static void main(String args[]) {
+        int num[] = { 2, 4, 6, 8, 10 };
         // reverseArray(num);
         // for(int i=0;i<num.length;i++)
         // {
-        //     System.out.print(num[i]+" ");
+        // System.out.print(num[i]+" ");
         // }
-        // System.out.println();
-        pairsInArray(num);
+        System.out.println();
+        subarray(num);
     }
 }
