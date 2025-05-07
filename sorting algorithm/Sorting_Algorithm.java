3ef27af8ed27f5552d1c373340ef508e98401240
ayush gajera <ayushgajera50@gmail.com>
2025-05-07 23:29:58 +0530

    Insertion Sort for array sorting

--- sorting algorithm/Sorting_Algorithm.java
+++ sorting algorithm/Sorting_Algorithm.java
@@ -1,7 +1,7 @@
 // Concept: 
 //large element come to the end of array by swapping with adjacent element
 
-public class bubbleSort {
+public class Sorting_Algorithm {
     public static void bubbleSort(int nums[]) {
         boolean swap = false;
         for (int turn = 0; turn < nums.length - 1; turn++) {
@@ -38,14 +38,36 @@ public class bubbleSort {
 
         }
     }
-
+    public static void insertionSort(int nums[])
+    {
+        for(int i=1;i<nums.length;i++)
+        {
+            int current=nums[i]; //store temparary variable
+            int prev=i-1;
+            while (prev>=0 && nums[prev]> current) {
+                nums[prev+1]=nums[prev];
+                prev--;
+            }
+            nums[prev+1]=current;
+        }
+    }
+    public static void printArray(int nums[])
+    {
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i]);
+        }
+    }
+    
+    
     public static void main(String[] args) {
         int nums[]={5,4,1,2,3};
         // int nums[] = { 1, 2, 3, 4, 5 };
         // bubbleSort(nums);
-        selectionSort(nums);
-        for (int i = 0; i < nums.length; i++) {
-            System.out.print(nums[i]);
-        }
+        // selectionSort(nums);
+        insertionSort(nums);
+         
+
+        
+        printArray(nums);
     }
 }
